bd9b82b273be450b878660612f9413db08c5e350
Rich Clingman <richclingman@gmail.com>
2023-08-23 09:54:58 -0500

    On square lines (2nd char group)

--- latLonToQth.js
+++ latLonToQth.js
@@ -1,16 +1,27 @@
 function latLonToQth(lat, lon) {
-    // latitude from south pole / 10
+
+    // TODO: Test for limits
+
+    // LATITUDE from south pole / 10
     const fullLat = lat + 90
     const lat1 = Math.floor(fullLat / 10)
-    const latField = String.fromCharCode(65 + lat1)
+    const latChar1 = String.fromCharCode(65 + lat1)
 
+    // remainder / 1 degrees
+    const latRem1 = fullLat - lat1 * 10
+    const lat2 = Math.floor(latRem1)
 
-    // longitude from anti-meridian / 20
+    // ******************
+    // LONGITUDE from anti-meridian / 20
     const fullLon = lon + 180
     const lon1 = Math.floor(fullLon / 20)
-    const lonField = String.fromCharCode(65 + lon1)
+    const lonChar1 = String.fromCharCode(65 + lon1)
 
-    const qth = `${lonField}${latField}00aa`
+    // remainder / 2 degrees
+    const lonrem1 = fullLon - lon1 * 20
+    const lon2 = Math.floor(lonrem1 / 2)
+
+    const qth = `${lonChar1}${latChar1}${lon2}${lat2}aa`
     console.log("lltq LAT:", lat, "LON:", lon, "QTH:", qth);
     return qth
 }
--- latLonToQth.test.js
+++ latLonToQth.test.js
@@ -6,6 +6,7 @@ console.log('package:', latLonToQth)
 
 // https://www.amsat.org/amsat-new/tools/grids.php <<< BAD RESULTS!!! (Recommended by ARRL)
 // https://www.giangrandi.org/electronics/radio/qthloccalc.shtml
+// http://www.vcars.org/tools-calculators/grid-square-calculator/
 
 describe('latLonToQth', () => {
     describe('on grid lines', () => {
@@ -16,6 +17,20 @@ describe('latLonToQth', () => {
             [90, 180, 'SS00aa'],
             [-90, 180, 'SA00aa'],
             [90, -180, 'AS00aa'],
+        ])('should map (%d, %d) to %s', (lat, lon, qth) => {
+            const resp = latLonToQth(lat, lon)
+            expect(resp).toBe(qth)
+        })
+    })
+
+    describe('on square lines', () => {
+        it.each([
+            [24, 46, 'LL34aa'],
+            [-87, -174, 'AA33aa'],
+            [5, 18, 'JJ95aa'],
+            [63, 174, 'RP73aa'],
+            [-45, 28, 'KE45aa'],
+            [71, -46, 'GQ71aa'],
         ])('should map (%d, %d) to %s', (lat, lon, qth) => {
             const resp = latLonToQth(lat, lon)
             expect(resp).toBe(qth)
